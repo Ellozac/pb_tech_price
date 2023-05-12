1eac578900a46ada19d1994527431d868959678f
Ellozac <zlsy108@gmail.com>
2023-05-13 10:08:54 +1200

    All working now!

--- adder.py
+++ adder.py
@@ -11,6 +11,7 @@ price = 0
 parent_span = ""
 pp_span = ""
 value = ""
+data = []
 
 class product:
     def __init__(self, name, url):
@@ -20,17 +21,39 @@ class product:
 
 
 cur.execute("CREATE TABLE IF NOT EXISTS wishlist(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, url TEXT NOT NULL, cost INTEGER NOT NULL);")
-
-userInput = input("Would you like to (1)add to the database or (2)view the database?\n> ")
-
-if userInput == "1":
-    newProd = product(input("What would you like to call this product?\n>"), input("Url for product?\n> "))
-    response = requests.get(newProd.url)
-    soup = BeautifulSoup(response.content, "html.parser")
-    pp_span = soup.find("span", {"class": "explist_dollars"}).text
-    pp_span = pp_span[1:]
-    pp_span = int(pp_span)
-    pp_span +=  pp_span * 0.15
-    pp_span = round(pp_span)
-    cur.execute("INSERT INTO wishlist(name, url, cost) VALUES(?,?,?);",(newProd.name, newProd.url,pp_span))
-    connection.commit()
+while True:
+    userInput = input("Would you like to (1)add to the database or (2)view the database (3) wipe database? (4) end program\n> ")
+    if userInput == "1":
+        newProd = product(input("What would you like to call this product?\n>"), input("Url for product?\n> "))
+        response = requests.get(newProd.url)
+        soup = BeautifulSoup(response.content, "html.parser")
+        pp_span = soup.find("span", {"class": "explist_dollars"}).text
+        pp_span = pp_span[1:]
+        pp_span = pp_span.replace(",", "")
+        pp_span = int(pp_span)
+        pp_span +=  pp_span * 0.15
+        pp_span = round(pp_span)
+        cur.execute("INSERT INTO wishlist(name, url, cost) VALUES(?,?,?);",(newProd.name, newProd.url,pp_span))
+        connection.commit()
+        
+        
+    if userInput == "2":
+        cur.execute("SELECT * FROM wishlist;")
+        data = cur.fetchall()
+        for row in data:
+            print(f"id: {row[0]} Name: {row[1]} URL: {row[2]} Price: ${row[3]}")
+            
+            
+    if userInput == "3":
+        userInput = input("1. Wipe\n2. Remove 1 item\n> ")
+        if userInput == "1":
+            cur.execute("DROP TABLE IF EXISTS wishlist")
+            connection.commit()
+            
+            
+        else:
+            userInput = input("What is the id of the item you would like to remove?\n> ")
+            cur.execute("DELETE FROM wishlist WHERE id = ?", (userInput))
+            connection.commit()
+    if userInput == "4":
+        break
